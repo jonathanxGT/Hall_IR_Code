163912d819ae53ba4eaff000166cd1afe08ca81b
Jonathan Ota <jonathanota1@gmail.com>
2015-07-16 11:56:09 -0700

    2D smoothing array
    changed smoothing array to deal with multiple sensors. changed
    lastDebounceTime to a local var. messing with the debounce function now.

--- sensor_array_setup/sensor_array_setup.ino
+++ sensor_array_setup/sensor_array_setup.ino
@@ -8,7 +8,7 @@ const byte hallEffectCount = 6;
 
 //setup IR Sensor array
 byte ir_sensor [] = {
-  2, 3
+  1, 2
 };
 const byte irSensorCount = 2;
 
@@ -22,7 +22,6 @@ const byte productNamesCount = 5;
 int prev_hall_readings [hallEffectCount];
 int prev_ir_readings [irSensorCount];
 
-long lastDebounceTime = 0;
 byte debounceDelay = 500;
 boolean movedForReal = false;
 boolean pickedUp = false;
@@ -46,39 +45,41 @@ void readSensors() {
     int currentIrRead;
     for (byte i = 3; i < hallEffectCount; i++) {
       currentHallRead = digitalRead(hall_effect[i]);
-      //smoothData(currentHallRead, prev_hall_readings[i], true);
+      //smoothData(currentHallRead, prev_hall_readings[i], i, true);
       prev_hall_readings[i] = currentHallRead;
 
     }
 
-    for (byte h = 2; h < irSensorCount; h ++) {
+    for (byte h = 1; h < irSensorCount; h ++) {
       currentIrRead = digitalRead(ir_sensor[h]);
-      //smoothData(currentIrRead, prev_ir_readings[h], false);
+      //smoothData(currentIrRead, prev_ir_readings[h], h, false);
       prev_ir_readings[h] = currentIrRead;
     }
 
 
   }
 
-void smoothData(int senseData, int prevSenseData, boolean sensorGate) {
+void smoothData(int senseData, int prevSenseData, byte index, boolean sensorGate) {
   int sensorData;
   int prevSensorData;
   int total,average;
+  byte arrayIndex;
   boolean hallEffect;
 
 //store passed down var's
   sensorData = senseData;
   prevSensorData = prevSenseData;
+  arrayIndex = index;
   hallEffect = sensorGate;
 
-//setup data smoothing array
-int totalSensorData [10];
+//setup 2D data smoothing array
+int totalSensorData [6][10];
 int totalSensorIndex = 0;
 
   //data smoothing
-  total = total - totalSensorData[totalSensorIndex];
-  totalSensorData[totalSensorIndex] = sensorData;
-  total = total + totalSensorData[totalSensorIndex];
+  total = total - totalSensorData[arrayIndex][totalSensorIndex];
+  totalSensorData[arrayIndex][totalSensorIndex] = sensorData;
+  total = total + totalSensorData[arrayIndex][totalSensorIndex];
 
   totalSensorIndex = totalSensorIndex + 1;
 
@@ -105,29 +106,19 @@ void hallDebounce(int avg, int prevData){
    int threshold = 0;
    int sensorData = avg;
    int prevSensorData = prevData;
-//   boolean movedForReal = false;
-//   boolean pickedUp = false;
-//   long lastDebounceTime;
+   long lastDebounceTime = 0;
    
-    if (sensorData - prevSensorData > threshold) {
+    if (abs(sensorData - prevSensorData) > threshold) {
      lastDebounceTime = millis();
-    movedForReal = true;
   }
   if (millis() - lastDebounceTime > debounceDelay) {
-    measuredWeight = totalWeight;
-    if (movedForReal) {
-      idWeight =  measuredWeight - previousMeasuredWeight;
-      if (idWeight > 0) {
+
         pickedUp = true;
-      }
-      else {
-        pickedUp = false;
-      }
+      
 //      checkObjects(abs(idWeight));
 //      previousMeasuredWeight = measuredWeight;
-      movedForReal = false;
+      
     }    
-  }
 }
 
 void irDebounce(int avg, int prevData){
