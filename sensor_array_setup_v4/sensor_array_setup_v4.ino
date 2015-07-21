045a3ef96ad9b898078187a25852cecf494bafa0
Jonathan Ota <jonathanota1@gmail.com>
2015-07-21 10:33:42 -0700

    SD lib
    adding SD code.

--- sensor_array_setup_v4/sensor_array_setup_v4.ino
+++ sensor_array_setup_v4/sensor_array_setup_v4.ino
@@ -1,4 +1,22 @@
 
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+#define LOG_INTERVAL  1000
+#define SYNC_INTERVAL 1000
+uint32_t syncTime = 0;
+uint32_t m = 0;
+
+#define ECHO_TO_SERIAL   1
+
+#define redLEDpin 2
+
+RTC_DS1307 RTC;
+
+const int chipSelect = 10;
+
 //---setup sensor pin array
 int sensor [] = {
   A1, A2, 3, 4, 5, 6, 7, 8
@@ -26,13 +44,22 @@ int allDataArray[sensorCount][3];
 unsigned long arrayLastDebounceTime [sensorCount];
 
 
-int debounceDelay = 750;
+int debounceDelay = 1000;
 byte arrayIndex;
 
-
+File logfile;
 void setup() {
 
   Serial.begin(9600);
+
+  //---- Logging setup ----//
+  // use debugging LEDs
+  pinMode(redLEDpin, OUTPUT);
+
+  logfile.println("millis,stamp,datetime,sensor name, status");
+#if ECHO_TO_SERIAL
+  Serial.println("millis,stamp,datetime,sensor name, status");
+#endif //ECHO_TO_SERIAL
 
   for (byte h = 0; h < sensorCount; h++) {
     pinMode(sensor[h], INPUT);
@@ -45,6 +72,7 @@ void setup() {
 }
 
 void loop() {
+  m = millis();
   readSensors();
 }
 
@@ -85,9 +113,9 @@ void smoothData(int sensorData, byte j) {
 
   average = allDataArray[arrayIndex] [0] / 10;
 
-//  Serial.print(arrayIndex);
-//  Serial.print(' ');
-//  Serial.println(average);
+  //  Serial.print(arrayIndex);
+  //  Serial.print(' ');
+  //  Serial.println(average);
 
 
   delay(1);
@@ -101,7 +129,7 @@ void debounceAndCheck(int avg) {
 
   int threshold;
   int hallThresh = 0;
-  int irThresh = 5;
+  int irThresh = 7;
   int sensorDifference;
   long debounceDifference;
 
@@ -111,34 +139,16 @@ void debounceAndCheck(int avg) {
     threshold = hallThresh;
   }
 
-
   //check difference to see if it's meaningful and if it's a pick or place
   sensorDifference = (avg - allDataArray[arrayIndex] [1]);
 
   //check if the IR sensor is a place and then adjust IR threshold for a place
-  if (arrayIndex < 2 ) {
+  if (arrayIndex < 2 && sensorDifference > 0) {
     threshold = irThresh;
   }
-  else if (arrayIndex < 2 ){   
+  else if (arrayIndex < 2 && sensorDifference < 0) {
     threshold = irThresh + 3;
   }
-
-  // delay(10);
-  
-//    if (arrayIndex == 0){
-//      Serial.print(arrayIndex);
-//      Serial.print(' ');
-//      Serial.print(threshold);
-//      Serial.print(' ');
-//      Serial.print(avg);
-//      Serial.print(' ');
-//      Serial.print(sensorDifference);
-//      Serial.print(' ');
-//      Serial.print(allDataArray[arrayIndex][1]);
-//      Serial.print(' ');
-//      Serial.println(allDataArray[arrayIndex][2]);
-//    }
-    
 
 
   if (abs(sensorDifference) > threshold) {
@@ -166,23 +176,138 @@ void debounceAndCheck(int avg) {
 
 }
 
-
 void logData(char *str, char *stat) {
 
-  Serial.print(str);
-  Serial.print(' ');
-  Serial.println(stat);
+  DateTime now;
+  
 
+  logfile.print(m);           // milliseconds since start
+  logfile.print(", ");
+
+#if ECHO_TO_SERIAL
+  Serial.print(m);         // milliseconds since start
+  Serial.print(", ");
+#endif
+
+  // fetch the time
+  now = RTC.now();
+
+  // log date
+  logfile.print(now.year(), DEC);
+  logfile.print("/");
+  logfile.print(now.month(), DEC);
+  logfile.print("/");
+  logfile.print(now.day(), DEC);
+  logfile.print(", ");
+
+  //log time
+  logfile.print(now.hour(), DEC);
+  logfile.print(":");
+  logfile.print(now.minute(), DEC);
+  logfile.print(":");
+  logfile.print(now.second(), DEC);
+
+  //log activity
+  logfile.print(", ");
+  logfile.print(' ');
+  logfile.print(", ");
+  logfile.print(' ');
+  logfile.print(", ");
+  logfile.print(' ');
+  logfile.println();
+
+#if ECHO_TO_SERIAL
+  Serial.print(now.year(), DEC);
+  Serial.print(F("/"));
+  Serial.print(now.month(), DEC);
+  Serial.print(F("/"));
+  Serial.print(now.day(), DEC);
+  Serial.print(F(", "));
+
+  Serial.print(now.hour(), DEC);
+  Serial.print(F(":"));
+  Serial.print(now.minute(), DEC);
+  Serial.print(F(":"));
+  Serial.print(now.second(), DEC);
+
+  Serial.print(F(", "));
+  Serial.print(' ');
+  Serial.print(F(", "));
+  Serial.print(' ');
+  Serial.print(F(", "));
+  Serial.print(' ');
+  Serial.println();
+#endif ECHO_TO_SERIAL
+
+  
+
+  // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
+  // which uses a bunch of power and takes time
+  if ((millis() - syncTime) < SYNC_INTERVAL) return;
+  syncTime = millis();
+
+  // blink LED to show we are syncing data to the card & updating FAT!
+  digitalWrite(redLEDpin, HIGH);
+  //digitalWrite(statusLED, HIGH);
+  logfile.flush();
+  //digitalWrite(statusLED, LOW);
+  digitalWrite(redLEDpin, LOW);
 }
 
-void codeDebug(long x, long y) {
-  Serial.print(arrayIndex);
-  Serial.print(' ');
-  Serial.print(x);
-  Serial.print(' ');
-  Serial.print(y);
-  Serial.print(' ');
-  Serial.println(productName[arrayIndex]);
+void error(char *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+
+  // red LED indicates error
+  digitalWrite(redLEDpin, HIGH);
+
+  while (1);
+}
+
+void initializeSD() {
+
+  // initialize the SD card
+  Serial.print(F("Initializing SD card..."));
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    error(F("Card failed, or not present"));
+  }
+  Serial.println(F("card initialized."));
+
+  // create a new file
+  char filename[] = "3DMP00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[4] = i / 10 + '0';
+    filename[5] = i % 10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE);
+      break;  // leave the loop!
+    }
+  }
+
+  if (! logfile) {
+    error(F("couldnt create file"));
+  }
+
+  Serial.print(F("Logging to: "));
+  Serial.println(filename);
+}
+
+void connectToRTC() {
+  // connect to RTC
+  Wire.begin();
+  if (!RTC.begin()) {
+    logfile.println("RTC failed");
+#if ECHO_TO_SERIAL
+    Serial.println("RTC failed");
+#endif  //ECHO_TO_SERIAL
+  }
 }
 
 
